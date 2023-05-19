cb896fb3215d4e52c4955cfd275251315ac4450b
Marius Ratnikas <marius.ratnikas@gmail.com>
2023-05-19 11:51:07 +0300

    first task

---
+++ users-script.js
@@ -0,0 +1,28 @@
+async function getServerData(dataForm) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/${dataForm}`);
+    const data = await res.json();
+    return data;
+}
+
+async function getUsersWithPosts() {
+    const dataForm = 'users?_embed=posts';
+    const users = await getServerData(dataForm);
+    return users;
+};
+
+
+(async function() {
+    const containerElement = document.querySelector('.container');
+    const UlElement = document.createElement('ul');
+    containerElement.append(UlElement)
+    const usersArr = await getUsersWithPosts();
+    usersArr.forEach(user => {
+        console.log(user)
+        const liElement = document.createElement('li');
+        const aElelemnt = document.createElement('a');
+        aElelemnt.textContent = `${user.name} (${user.posts.length})`;
+        aElelemnt.href = './user.html';
+        liElement.append(aElelemnt)
+        UlElement.append(liElement)
+    })
+})()
